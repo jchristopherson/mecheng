26c60d5f81ce477efa47e2aa60ab2f14b77617b3
jchristopherson <jchristopherson@hotmail.com>
2018-01-01 14:48:28 -0600

    Add signals module

---
+++ src/signals.f90
@@ -0,0 +1,13 @@
+! signals.f90
+
+!> @brief \b signals
+!!
+!! @par Purpose
+!! This module provides a small collection of basic signal processing routines.
+module signals
+    use, intrinsic :: iso_fortran_env, only : int32, real64
+    implicit none
+    private
+
+contains
+end module
