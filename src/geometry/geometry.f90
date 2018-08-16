596c218c047e9d7f6b3c624d9f4dcbb45017cc55
Jason Christopherson <jchristopherson@hotmail.com>
2018-08-16 06:53:41 -0500

    Update geometry module

--- src/geometry/geometry.f90
+++ src/geometry/geometry.f90
@@ -3,33 +3,7 @@
 module geometry
     use iso_fortran_env
     implicit none
-    private
-    public :: vector
-    public :: cross
 
-! ******************************************************************************
-! TYPES
-! ------------------------------------------------------------------------------
-    !> @brief Defines a 3-component, Cartesian vector.
-    type vector
-        !> The x-component of the vector.
-        real(real64) :: x
-        !> The y-component of the vector.
-        real(real64) :: y
-        !> The z-component of the vector.
-        real(real64) :: z
-    end type
-
-! ******************************************************************************
-! INTERFACES
-! ------------------------------------------------------------------------------
-    !> @brief Computes the cross-product of a vector.
-    interface cross
-        module procedure :: cross_1
-        module procedure :: cross_2
-    end interface
-
-! ------------------------------------------------------------------------------
 contains
 ! ******************************************************************************
 ! VECTOR RELATED ROUTINES
@@ -39,42 +13,37 @@ contains
     !! @param[in] x The left-hand-side operator.
     !! @param[in] y The right-hand-side operator.
     !! @return The resulting vector.
-    pure function cross_1(x, y) result(z)
+    pure function cross(x, y) result(z)
         ! Arguments
-        class(vector), intent(in) :: x, y
-        type(vector) :: z
+        real(real64), intent(in), dimension(3) :: x, y
+        real(real64), dimension(3) :: z
 
         ! Process
-        z%x = x%y * y%z - x%z * y%y
-        z%y = x%z * y%x - x%x * y%z
-        z%z = x%x * y%y - x%y * y%x
+        z(1) = x(2) * y(3) - x(3) * y(2)
+        z(2) = x(3) * y(1) - x(1) * y(3)
+        z(3) = x(1) * y(2) - x(2) * y(1)
     end function
-
-! --------------------
-    !> @brief Computes the cross-product of a vector.
-    !!
-    !! @param[in] x The left-hand-side operator.
-    !! @param[in] y The right-hand-side operator.
-    !! @return The resulting vector.
-    pure function cross_2(x, y) result(z)
-        ! Arguments
-        class(vector), intent(in) :: x
-        real(real64), intent(in), dimension(3) :: y
-        type(vector) :: z
-
-        ! Process
-        z%x = x%y * y(3) - x%z * y(2)
-        z%y = x%z * y(1) - x%x * y(3)
-        z%z = x%x * y(2) - x%y * y(1)
-    end function
-
-! --------------------
-
-! --------------------
-
-! --------------------
 
 ! ------------------------------------------------------------------------------
+    !> @brief Computes the projection of a vector onto another vector.
+    !!
+    !! @param[in] x The vector to project.
+    !! @param[in] y The vector onto which @p x is to be projected.
+    !! @return The projected vector.
+    pure function proj(x, y) result(z)
+        ! Arguments
+        real(real64), intent(in), dimension(3) :: x, y
+        real(real64), dimension(3) :: z
+
+        ! Local Variables
+        real(real64) :: x1
+
+        ! Compute the scalar projection
+        x1 = dot_product(x, y) / norm2(y)
+
+        ! Compute the projection
+        z = x1 * y
+    end function
 
 ! ------------------------------------------------------------------------------
 
