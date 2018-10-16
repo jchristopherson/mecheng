72451cfe29174a377253624330d7c0211264d0bd
Jason Christopherson <jchristopherson@hotmail.com>
2018-10-16 15:12:55 -0500

    Define test application for neural_network code

---
+++ tests/neural_network/test_neural_network.f90
@@ -0,0 +1,21 @@
+! test_neural_network.f90
+
+program main
+    use test_layers
+
+    ! Local Variables
+    logical :: rst, overall
+
+    ! Process
+    overall = .true.
+
+    rst = test_layer()
+    if (.not.rst) overall = .false.
+
+    ! End
+    if (rst) then
+        print '(A)', "NEURAL_NETWORK TESTS PASSED."
+    else
+        print '(A)', "NEURAL_NETWORK TESTS FAILED."
+    end if
+end program
