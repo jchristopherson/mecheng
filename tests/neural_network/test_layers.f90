3796fde350cbf4b0aa4e5e0502ddc136f16506b7
Jason Christopherson <jchristopherson@hotmail.com>
2018-10-17 07:50:39 -0500

    Add layer tests

--- tests/neural_network/test_layers.f90
+++ tests/neural_network/test_layers.f90
@@ -10,11 +10,14 @@ contains
         ! Parameters
         integer(int32), parameter :: nNeurons = 100
         integer(int32), parameter :: nInputs = 500
+        real(real64), parameter :: tol = 1.0d-12
 
         ! Local Variables
         logical :: rst
         type(layer) :: lyr
-        real(real64), allocatable :: b(:), w(:,:)
+        real(real64), allocatable :: b(:), w(:,:), z(:), zc(:), a(:), ac(:), da(:), dc(:)
+        real(real64) :: x(nInputs)
+        integer(int32) :: i
 
         ! Initialization
         rst = .true.
@@ -60,5 +63,61 @@ contains
         end if
 
         ! Test w * x + b
+        call random_number(x)
+        zc = matmul(w, x) + b
+        z = lyr%eval_arguments(x)
+
+        if (size(z) /= nNeurons) then
+            rst = .false.
+            print '(AI0AI0A)', "TEST FAILED (TEST_LAYER): The neural output array is " // &
+                "incorrectly sized.  Expected an array of ", nNeurons, &
+                " elements, but found an array of ", size(z), " elements."
+            return
+        end if
+        do i = 1, nNeurons
+            if (abs(z(i) - zc(i)) > tol) then
+                rst = .false.
+                print '(A)', "TEST FAILED (TEST_LAYER): The neural output is incorrect."
+                return
+            end if
+        end do
+
+        ! Test the sigmoid function evaluation
+        ac = sigmoid(zc)
+        a = lyr%eval_neural_function(z)
+
+        if (size(a) /= nNeurons) then
+            rst = .false.
+            print '(AI0AI0A)', "TEST FAILED (TEST_LAYER): The neural function array is " // &
+                "incorrectly sized.  Expected an array of ", nNeurons, &
+                " elements, but found an array of ", size(a), " elements."
+            return
+        end if
+        do i = 1, nNeurons
+            if (abs(a(i) - ac(i)) > tol) then
+                rst = .false.
+                print '(A)', "TEST FAILED (TEST_LAYER): The neural function output is incorrect."
+                return
+            end if
+        end do
+
+        ! Test the derivative evaluation
+        dc = sigmoid_derivative(zc)
+        da = lyr%eval_neural_derivative(z)
+
+        if (size(da) /= nNeurons) then
+            rst = .false.
+            print '(AI0AI0A)', "TEST FAILED (TEST_LAYER): The neural derivative array is " // &
+                "incorrectly sized.  Expected an array of ", nNeurons, &
+                " elements, but found an array of ", size(da), " elements."
+            return
+        end if
+        do i = 1, nNeurons
+            if (abs(da(i) - dc(i)) > tol) then
+                rst = .false.
+                print '(A)', "TEST FAILED (TEST_LAYER): The neural derivative output is incorrect."
+                return
+            end if
+        end do
     end function
 end module
