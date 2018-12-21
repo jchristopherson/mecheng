a41bcb76d45860b6eb008a65c5b792cf2650bad2
Jason Christopherson <jchristopherson@hotmail.com>
2018-12-21 09:33:37 -0600

    Add frequency response calculations

--- src/mechanics/vibrations.f90
+++ src/mechanics/vibrations.f90
@@ -12,11 +12,11 @@ module vibrations
     public :: modal_information
     public :: compute_poincare_section
     public :: compute_modal_response
+    public :: compute_frequency_response
 
     ! TO DO:
     ! - Primary Oscillation Frequency Finder (Base upon an FFT, and return the largest magnitude non-DC frequency)
     ! - Frequency Sweep Type FRF's
-    ! - Modal Analysis
 
     !> @brief Contains modal information such as frequency and mode shape.
     type modal_information
@@ -25,6 +25,13 @@ module vibrations
         !> The mode shape.
         real(real64), allocatable, dimension(:) :: mode_shape
     end type
+
+    !> @brief Compute the frequency response of a linear vibrating system.
+    interface compute_frequency_response
+        module procedure :: compute_frequency_response_1
+        module procedure :: compute_frequency_response_2
+        module procedure :: compute_frequency_response_3
+    end interface
 
 contains
 ! ------------------------------------------------------------------------------
@@ -233,6 +240,373 @@ contains
             val = rst(i)%mode_shape(ind)
             rst(i)%mode_shape = rst(i)%mode_shape / val
         end do
+    end function
+
+! ------------------------------------------------------------------------------
+    !> @brief Compute the frequency response of a linear vibrating system.
+    !!
+    !! @param[in] m An N-by-N mass matrix.
+    !! @param[in] k An N-by-N stiffness matrix.
+    !! @param[in] b An N-by-N damping matrix.
+    !! @param[in] f An M-by-N forcing function matrix with each of the M
+    !!  rows representing a discrete excitation frequency.
+    !! @param[in] freq An M-element array containing the frequency points
+    !!  at which to analyze the system.  The units are expected to be rad/s.
+    !! @param[in,out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - MECH_ARRAY_SIZE_ERROR: Occurs if any of the input matrices are not 
+    !!      square, or if the matrices are sized differently.
+    !!  - MECH_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory 
+    !!      available.
+    !!  - LA_CONVERGENCE_ERROR: Occurs if the eigen solver fails to converge.
+    !!
+    !! @return An M-by-N element matrix where each of the N results
+    !!  can be written for all M frequency points of interest.
+    function compute_frequency_response_1(m, k, b, f, freq, err) result(rsp)
+        ! Arguments
+        real(real64), intent(in), dimension(:,:) :: m, k, b
+        complex(real64), intent(in), dimension(:,:) :: f
+        real(real64), intent(in), dimension(:) :: freq
+        class(errors), intent(inout), optional, target :: err
+        complex(real64), allocatable, dimension(:,:) :: rsp
+
+        ! Parameters
+        complex(real64), parameter :: ci = (0.0d0, 1.0d0)
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+        character(len = 256) :: errmsg
+        integer(int32) :: i, j, nfreq, neqn, flag
+        real(real64), allocatable, dimension(:,:) :: mc, kc
+        complex(real64), allocatable, dimension(:) :: vals, s
+        complex(real64), allocatable, dimension(:,:) :: vecs, &
+            vectrans, mm, km, bm, fm, q
+        
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        neqn = size(m, 1)
+        nfreq = size(freq)
+
+        ! Array Size Check
+        if (size(m, 2) /= neqn) then
+            call errmgr%report_error("compute_frequency_response_1", &
+                "The mass matrix is not square.", MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        if (size(k, 1) /= neqn .or. size(k, 2) /= neqn) then
+            write(errmsg, '(AI0AI0AI0AI0A)') &
+                "The stiffness matrix is not sized properly.  Expected a ", &
+                neqn, "-by-", neqn, " matrix, but found a ", size(k, 1), &
+                "-by-", size(k, 2), " matrix."
+            call errmgr%report_error("compute_frequency_response_1", &
+                trim(errmsg), MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        if (size(b, 1) /= neqn .or. size(b, 2) /= neqn) then
+            write(errmsg, '(AI0AI0AI0AI0A)') &
+                "The damping matrix is not sized properly.  Expected a ", &
+                neqn, "-by-", neqn, " matrix, but found a ", size(b, 1), &
+                "-by-", size(b, 2), " matrix."
+            call errmgr%report_error("compute_frequency_response_1", &
+                trim(errmsg), MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        ! Local Memory Allocation
+        allocate(mc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(kc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(vals(neqn), stat = flag)
+        if (flag == 0) allocate(vecs(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(q(nfreq, neqn), stat = flag)
+        if (flag /= 0) then
+            call errmgr%report_error("compute_frequency_response_1", &
+                "Insufficient memory available.", MECH_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+
+        ! Create a copy of the mass and stiffness matrices such that the
+        ! eigen solver does not overwrite the originals
+        do j = 1, neqn
+            do i = 1, neqn
+                mc(i,j) = m(i,j)
+                kc(i,j) = k(i,j)
+            end do
+        end do
+
+        ! Compute the eigenvalues and eigenvectors
+        call eigen(kc, mc, vals, vecs = vecs, err = errmgr)
+        if (errmgr%has_error_occurred()) return
+
+        ! Compute the modal matrices
+        vectrans = transpose(vecs)
+        mm = matmul(vectrans, matmul(m, vecs))
+        km = matmul(vectrans, matmul(k, vecs))
+        bm = matmul(vectrans, matmul(b, vecs))
+        fm = matmul(f, vecs)
+
+        ! Compute the complex frequency term
+        s = ci * freq
+
+        ! Compute the FRF of each decoupled equation
+        do j = 1, neqn
+            q(:,j) = fm(:,j) / (mm(j,j) * s**2 + bm(j,j) * s + km(j,j))
+        end do
+
+        ! Convert back to the original, coupled configuration
+        rsp = matmul(q, vectrans)
+    end function
+
+! --------------------
+    !> @brief Compute the frequency response of a linear vibrating system.
+    !!
+    !! @param[in] m An N-by-N mass matrix.
+    !! @param[in] k An N-by-N stiffness matrix.
+    !! @param[in] zeta An N-element array containing a damping ratio for
+    !!  each of the N equations.
+    !! @param[in] f An M-by-N forcing function matrix with each of the M
+    !!  rows representing a discrete excitation frequency.
+    !! @param[in] freq An M-element array containing the frequency points
+    !!  at which to analyze the system.  The units are expected to be rad/s.
+    !! @param[in,out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - MECH_ARRAY_SIZE_ERROR: Occurs if any of the input matrices are not 
+    !!      square, or if the matrices are sized differently.
+    !!  - MECH_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory 
+    !!      available.
+    !!  - LA_CONVERGENCE_ERROR: Occurs if the eigen solver fails to converge.
+    !!
+    !! @return An M-by-N element matrix where each of the N results
+    !!  can be written for all M frequency points of interest.
+    function compute_frequency_response_2(m, k, zeta, f, freq, err) result(rsp)
+        ! Arguments
+        real(real64), intent(in), dimension(:,:) :: m, k
+        complex(real64), intent(in), dimension(:,:) :: f
+        real(real64), intent(in), dimension(:) :: zeta, freq
+        class(errors), intent(inout), optional, target :: err
+        complex(real64), allocatable, dimension(:,:) :: rsp
+
+        ! Parameters
+        complex(real64), parameter :: ci = (0.0d0, 1.0d0)
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+        character(len = 256) :: errmsg
+        integer(int32) :: i, j, nfreq, neqn, flag
+        real(real64), allocatable, dimension(:,:) :: mc, kc
+        complex(real64), allocatable, dimension(:) :: vals, s
+        complex(real64), allocatable, dimension(:,:) :: vecs, &
+            vectrans, mm, km, fm, q
+        complex(real64) :: b
+        
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        neqn = size(m, 1)
+        nfreq = size(freq)
+
+        ! Array Size Check
+        if (size(m, 2) /= neqn) then
+            call errmgr%report_error("compute_frequency_response_2", &
+                "The mass matrix is not square.", MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        if (size(k, 1) /= neqn .or. size(k, 2) /= neqn) then
+            write(errmsg, '(AI0AI0AI0AI0A)') &
+                "The stiffness matrix is not sized properly.  Expected a ", &
+                neqn, "-by-", neqn, " matrix, but found a ", size(k, 1), &
+                "-by-", size(k, 2), " matrix."
+            call errmgr%report_error("compute_frequency_response_2", &
+                trim(errmsg), MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        if (size(zeta) /= neqn) then
+            write(errmsg, '(AI0AI0A)') &
+                "The damping ratio vector is not sized properly.  " // &
+                "Expected an array of ", neqn, &
+                " elements, but found an array of ", size(zeta), &
+                " elements."
+            call errmgr%report_error("compute_frequency_response_2", &
+                trim(errmsg), MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        ! Local Memory Allocation
+        allocate(mc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(kc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(vals(neqn), stat = flag)
+        if (flag == 0) allocate(vecs(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(q(nfreq, neqn), stat = flag)
+        if (flag /= 0) then
+            call errmgr%report_error("compute_frequency_response_2", &
+                "Insufficient memory available.", MECH_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+
+        ! Create a copy of the mass and stiffness matrices such that the
+        ! eigen solver does not overwrite the originals
+        do j = 1, neqn
+            do i = 1, neqn
+                mc(i,j) = m(i,j)
+                kc(i,j) = k(i,j)
+            end do
+        end do
+
+        ! Compute the eigenvalues and eigenvectors
+        call eigen(kc, mc, vals, vecs = vecs, err = errmgr)
+        if (errmgr%has_error_occurred()) return
+
+        ! Compute the modal matrices
+        vectrans = transpose(vecs)
+        mm = matmul(vectrans, matmul(m, vecs))
+        km = matmul(vectrans, matmul(k, vecs))
+        fm = matmul(f, vecs)
+
+        ! Compute the complex frequency term
+        s = ci * freq
+
+        ! Compute the FRF of each decoupled equation
+        do j = 1, neqn
+            b = 2.0d0 * zeta(j) * sqrt(km(j,j) * mm(j,j))
+            q(:,j) = fm(:,j) / (mm(j,j) * s**2 + b * s + km(j,j))
+        end do
+
+        ! Convert back to the original, coupled configuration
+        rsp = matmul(q, vectrans) 
+    end function
+
+! --------------------
+    !> @brief Compute the frequency response of a linear vibrating system.
+    !!
+    !! @param[in] m An N-by-N mass matrix.
+    !! @param[in] k An N-by-N stiffness matrix.
+    !! @param[in] zeta A damping ratio for each of the N equations.
+    !! @param[in] f An M-by-N forcing function matrix with each of the M
+    !!  rows representing a discrete excitation frequency.
+    !! @param[in] freq An M-element array containing the frequency points
+    !!  at which to analyze the system.  The units are expected to be rad/s.
+    !! @param[in,out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - MECH_ARRAY_SIZE_ERROR: Occurs if any of the input matrices are not 
+    !!      square, or if the matrices are sized differently.
+    !!  - MECH_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory 
+    !!      available.
+    !!  - LA_CONVERGENCE_ERROR: Occurs if the eigen solver fails to converge.
+    !!
+    !! @return An M-by-N element matrix where each of the N results
+    !!  can be written for all M frequency points of interest.
+    function compute_frequency_response_3(m, k, zeta, f, freq, err) result(rsp)
+        ! Arguments
+        real(real64), intent(in), dimension(:,:) :: m, k
+        real(real64), intent(in) :: zeta
+        complex(real64), intent(in), dimension(:,:) :: f
+        real(real64), intent(in), dimension(:) :: freq
+        class(errors), intent(inout), optional, target :: err
+        complex(real64), allocatable, dimension(:,:) :: rsp
+
+        ! Parameters
+        complex(real64), parameter :: ci = (0.0d0, 1.0d0)
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+        character(len = 256) :: errmsg
+        integer(int32) :: i, j, nfreq, neqn, flag
+        real(real64), allocatable, dimension(:,:) :: mc, kc
+        complex(real64), allocatable, dimension(:) :: vals, s
+        complex(real64), allocatable, dimension(:,:) :: vecs, &
+            vectrans, mm, km, fm, q
+        complex(real64) :: b
+        
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        neqn = size(m, 1)
+        nfreq = size(freq)
+
+        ! Array Size Check
+        if (size(m, 2) /= neqn) then
+            call errmgr%report_error("compute_frequency_response_3", &
+                "The mass matrix is not square.", MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        if (size(k, 1) /= neqn .or. size(k, 2) /= neqn) then
+            write(errmsg, '(AI0AI0AI0AI0A)') &
+                "The stiffness matrix is not sized properly.  Expected a ", &
+                neqn, "-by-", neqn, " matrix, but found a ", size(k, 1), &
+                "-by-", size(k, 2), " matrix."
+            call errmgr%report_error("compute_frequency_response_3", &
+                trim(errmsg), MECH_ARRAY_SIZE_ERROR)
+            return
+        end if
+
+        ! Local Memory Allocation
+        allocate(mc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(kc(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(vals(neqn), stat = flag)
+        if (flag == 0) allocate(vecs(neqn, neqn), stat = flag)
+        if (flag == 0) allocate(q(nfreq, neqn), stat = flag)
+        if (flag /= 0) then
+            call errmgr%report_error("compute_frequency_response_3", &
+                "Insufficient memory available.", MECH_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+
+        ! Create a copy of the mass and stiffness matrices such that the
+        ! eigen solver does not overwrite the originals
+        do j = 1, neqn
+            do i = 1, neqn
+                mc(i,j) = m(i,j)
+                kc(i,j) = k(i,j)
+            end do
+        end do
+
+        ! Compute the eigenvalues and eigenvectors
+        call eigen(kc, mc, vals, vecs = vecs, err = errmgr)
+        if (errmgr%has_error_occurred()) return
+
+        ! Compute the modal matrices
+        vectrans = transpose(vecs)
+        mm = matmul(vectrans, matmul(m, vecs))
+        km = matmul(vectrans, matmul(k, vecs))
+        fm = matmul(f, vecs)
+
+        ! Compute the complex frequency term
+        s = ci * freq
+
+        ! Compute the FRF of each decoupled equation
+        do j = 1, neqn
+            b = 2.0d0 * zeta * sqrt(km(j,j) * mm(j,j))
+            q(:,j) = fm(:,j) / (mm(j,j) * s**2 + b * s + km(j,j))
+        end do
+
+        ! Convert back to the original, coupled configuration
+        rsp = matmul(q, vectrans) 
     end function
 
 ! ------------------------------------------------------------------------------
