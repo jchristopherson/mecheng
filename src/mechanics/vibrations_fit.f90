0bbfdb3af1af6838cdc44cb1f2648baae2d83871
Jason Christopherson <720562@ad.seagate.com>
2019-10-02 15:09:27 -0500

    Add references

--- src/mechanics/vibrations_fit.f90
+++ src/mechanics/vibrations_fit.f90
@@ -5,6 +5,22 @@ contains
 ! ------------------------------------------------------------------------------
     ! Takes a single step of the fitting algorithm.
     !
+    ! References:
+    ! 1. B. Gustavsen and A. Semlyen, "Rational approximation of frequency 
+    !    domain responses by Vector Fitting", IEEE Trans. Power Delivery, 
+    !    vol. 14, no. 3, pp. 1052-1061, July 1999. Link
+    ! 2. B. Gustavsen, "Improving the pole relocating properties of vector 
+    !    fitting", IEEE Trans. Power Delivery, vol. 21, no. 3, pp. 1587-1592, 
+    !    July 2006. Link 
+    ! 3. D. Deschrijver, M. Mrozowski, T. Dhaene, and D. De Zutter, 
+    !    “Macromodeling of  Multiport Systems Using a Fast Implementation of 
+    !    the Vector Fitting Method”, IEEE Microwave and Wireless Components 
+    !    Letters, vol. 18, no. 6, pp. 383-385, June 2008.
+    !
+    ! Websites:
+    ! - https://www.sintef.no/vectfit
+    !
+    ! Arguments:
     ! - f: An M-element array containing the complex-valued frequency response
     !       function to fit.
     ! - s: An M-element array containing the complex-valued frequency points at
