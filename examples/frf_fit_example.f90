0d9563d456931459810a785568f0acb509d38c55
Jason Christopherson <720562@ad.seagate.com>
2019-10-08 09:18:17 -0500

    Update example

--- examples/frf_fit_example.f90
+++ examples/frf_fit_example.f90
@@ -64,7 +64,7 @@ program example
     print '(AF5.1A)', "Mode 2: ", info(2)%frequency, " Hz"
 
     ! Attempt to fit the FRF
-    call fit%fit(omega, frf_mag, frf_phase * pi / 1.8d2, 4)
+    call fit%fit(omega, frf_mag, frf_phase * pi / 1.8d2, 20)
 
     ! Extract the magnitude and phase components
     fit_mag = abs(fit%frf)
