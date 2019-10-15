6fec519171fbac7e2b3adf3eadf5473c51496494
Jason Christopherson <720562@ad.seagate.com>
2019-10-15 14:48:06 -0500

    Clean up

--- doc/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['a',['a',['../structvibrations_1_1state__space.html#a59c151552721ada96f505bdf1b703c98',1,'vibrations::state_space::a()'],['../structgeometry_1_1plane.html#a076727730a32666d8ba5d2ecc87e1ddc',1,'geometry::plane::a()'],['../structgeometry_1_1line.html#ab9fedac6563ab0a79d6e067b93c05f37',1,'geometry::line::a()']]]
-];
