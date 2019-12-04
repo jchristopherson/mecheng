60efb8535e627e2a4e9b37c61327bb720a180b82
Jason Christopherson <jason.christopherson@seagate.com>
2019-12-04 11:30:59 -0600

    Add measurements module

---
+++ src/general/measurements.f90
@@ -0,0 +1,9 @@
+! measurements.f90
+
+!> @brief Provides a collection of routines for operating on 
+!! experimental measurement data.
+module measurements
+    use iso_fortran_env
+    use curvefit_statistics
+    implicit none
+end module
