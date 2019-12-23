d5ed79ecae11aaa84446b7b492fbf2299997bf74
Jason Christopherson <jchristopherson@hotmail.com>
2019-12-23 10:31:16 -0600

    Add GR&R plotting abilities

--- src/general/measurements.f90
+++ src/general/measurements.f90
@@ -6,6 +6,7 @@ module measurements
     use iso_fortran_env
     use curvefit_statistics
     use ferror
+    use fplot_core
     implicit none
     private
     public :: MS_INVALID_INPUT_ERROR
@@ -18,6 +19,7 @@ module measurements
     public :: ems_gauge_r_r
     public :: emp_gauge_r_r
     public :: discrimination_ratio
+    public :: plot_grr
 
     !> @brief Defines an invalid input error condition.
     integer(int32), parameter :: MS_INVALID_INPUT_ERROR = 100001
@@ -564,6 +566,218 @@ contains
         real(real64) :: x
         x = sqrt(2.0d0 * (tv / mv) - 1.0d0)
     end function
+
+! ------------------------------------------------------------------------------
+    !> @brief Creates a GR&R plot.
+    !!
+    !! @param[in] x The M-by-N-by-P GR&R data matrix where M is the number of
+    !!  parts, N is the number of tests, and P is the number of operators.
+    !! @param[in,out] plt The plot object to which the data will be added.
+    !! @param[in,out] err An optional errors-based object that if provided can be
+    !!  used to retrieve information relating to any errors encountered during
+    !!  execution.  If not provided, a default implementation of the errors
+    !!  class is used internally to provide error handling.  Possible errors and
+    !!  warning messages that may be encountered are as follows.
+    !!  - MS_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory available.
+    !!
+    !! @par Example
+    !! @code{.f90}
+    !! program example
+    !!     use iso_fortran_env
+    !!     use measurements
+    !!     use fplot_core
+    !!     implicit none
+    !!
+    !!     ! Parameters
+    !!     real(real64), parameter :: tol = 1.0d3
+    !!
+    !!     ! Variables
+    !!     real(real64), allocatable, dimension(:,:,:) :: x
+    !!     type(ems_grr_results) :: ems
+    !!     type(emp_grr_results) :: emp
+    !!     type(plot_2d) :: plt
+    !!     class(plot_axis), pointer :: xAxis, yAxis
+    !!     class(legend), pointer :: lgnd
+    !!
+    !!     ! Populate the data set
+    !!     x = format_data()
+    !!
+    !!     ! Compute the GR&R using the EMS approach
+    !!     ems = ems_gauge_r_r(x, tol = tol)
+    !!
+    !!     ! Compute the GR&R using the EMP approach
+    !!     emp = emp_gauge_r_r(x, tol = tol)
+    !!
+    !!     ! Compare results
+    !!     print '(A)', "EMS Method"
+    !!     print '(AE10.3AF5.2A)', "Repeatability: ", ems%repeatability, &
+    !!         " (", 1.0d2 * ems%repeatability / ems%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Reproducibility: ", ems%reproducibility, &
+    !!         " (", 1.0d2 * ems%reproducibility / ems%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Gauge Variance: ", ems%gauge_variation, &
+    !!         " (", 1.0d2 * ems%gauge_variation / ems%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Part-Part Variance: ", ems%part_variation, &
+    !!         " (", 1.0d2 * ems%part_variation / ems%total_variation, "%)"
+    !!     print '(AE10.3)', "Total Variance: ", ems%total_variation
+    !!     print '(AF5.3)', "P/T Ratio: ", ems%pt_ratio
+    !!     print '(AF5.3)', "P/TV Ratio: ", ems%ptv_ratio
+    !!
+    !!     print '(A)', new_line('a') // "EMP Method"
+    !!     print '(AE10.3AF5.2A)', "Repeatability: ", emp%repeatability, &
+    !!         " (", 1.0d2 * emp%repeatability / emp%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Reproducibility: ", emp%reproducibility, &
+    !!         " (", 1.0d2 * emp%reproducibility / emp%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Gauge Variance: ", emp%gauge_variation, &
+    !!         " (", 1.0d2 * emp%gauge_variation / emp%total_variation, "%)"
+    !!     print '(AE10.3AF5.2A)', "Part-Part Variance: ", emp%part_variation, &
+    !!         " (", 1.0d2 * emp%part_variation / emp%total_variation, "%)"
+    !!     print '(AE10.3)', "Total Variance: ", emp%total_variation
+    !!     print '(AF5.3)', "P/T Ratio: ", emp%pt_ratio
+    !!     print '(AF5.3)', "P/TV Ratio: ", emp%ptv_ratio
+    !!
+    !!     ! Create a plot of the data
+    !!     call plt%initialize()
+    !!     call plt%set_font_size(14)
+    !!     call plt%set_show_gridlines(.false.)
+    !!
+    !!     xAxis => plt%get_x_axis()
+    !!     yAxis => plt%get_y_axis()
+    !!
+    !!     call xAxis%set_title("Part No.")
+    !!     call yAxis%set_title("Measured")
+    !!
+    !!     lgnd => plt%get_legend()
+    !!     call lgnd%set_is_visible(.true.)
+    !!
+    !!     call plot_grr(x, plt)
+    !!
+    !!     ! Show the plot
+    !!     call plt%draw()
+    !!
+    !! contains
+    !!     ! The data set is # of parts -by- # of tests -by- # of operators
+    !!     function format_data() result(x)
+    !!         ! Arguments
+    !!         real(real64), allocatable, dimension(:,:,:) :: x
+    !!
+    !!         ! Parameters
+    !!         integer(int32), parameter :: nparts = 9
+    !!         integer(int32), parameter :: nops = 2
+    !!         integer(int32), parameter :: ntests = 3
+    !!
+    !!         ! Local Variables
+    !!         real(real64), allocatable, dimension(:) :: op
+    !!
+    !!         ! Allocate space for the output
+    !!         allocate(x(nparts, ntests, nops))
+    !!         allocate(op(nparts * ntests))
+    !!
+    !!         ! Fill in the data for operator 1
+    !!         op = [39736.0d0, 40088.0d0, 39952.0d0, 39840.0d0, &
+    !!             39792.0d0, 39968.0d0, 39384.0d0, 39648.0d0, &
+    !!             39704.0d0, 39152.0d0, 39240.0d0, 39312.0d0, &
+    !!             38832.0d0, 38688.0d0, 38704.0d0, 38936.0d0, &
+    !!             38872.0d0, 38928.0d0, 39040.0d0, 38952.0d0, &
+    !!             38968.0d0, 39128.0d0, 38928.0d0, 38928.0d0, &
+    !!             38712.0d0, 38688.0d0, 38624.0d0]
+    !!         x(:,:,1) = transpose(reshape(op, [ntests, nparts]))
+    !!
+    !!         ! Fill in the data for operator 2
+    !!         op = [40256.0d0, 39864.0d0, 39792.0d0, 39816.0d0, &
+    !!             39888.0d0, 39784.0d0, 39760.0d0, 39944.0d0, &
+    !!             39832.0d0, 39272.0d0, 39288.0d0, 39296.0d0, &
+    !!             38728.0d0, 38768.0d0, 38704.0d0, 38896.0d0, &
+    !!             38952.0d0, 38968.0d0, 39032.0d0, 38968.0d0, &
+    !!             38992.0d0, 39032.0d0, 39008.0d0, 38968.0d0, &
+    !!             38592.0d0, 38640.0d0, 38584.0d0]
+    !!         x(:,:,2) = transpose(reshape(op, [ntests, nparts]))
+    !!     end function
+    !! end program
+    !! @endcode
+    !! The above code produces the following output.
+    !! @code{.txt}
+    !! EMS Method
+    !! Repeatability:  0.999E+04 ( 4.00%)
+    !! Reproducibility:  0.127E+04 ( 0.51%)
+    !! Gauge Variance:  0.113E+05 ( 4.52%)
+    !! Part-Part Variance:  0.238E+06 (95.48%)
+    !! Total Variance:  0.250E+06
+    !! P/T Ratio: 0.637
+    !! P/TV Ratio: 0.045
+    !!
+    !! EMP Method
+    !! Repeatability:  0.778E+04 ( 3.84%)
+    !! Reproducibility:  0.835E+03 ( 0.41%)
+    !! Gauge Variance:  0.862E+04 ( 4.25%)
+    !! Part-Part Variance:  0.194E+06 (95.75%)
+    !! Total Variance:  0.203E+06
+    !! P/T Ratio: 0.557
+    !! P/TV Ratio: 0.043
+    !! @endcode
+    !! @image html grr_plot_example.png
+    subroutine plot_grr(x, plt, err)
+        use curvefit_statistics
+        use strings
+
+        ! Arguments
+        real(real64), intent(in), dimension(:,:,:) :: x
+        class(plot), intent(inout) :: plt
+        class(errors), intent(inout), optional, target :: err
+
+        ! Local Variables
+        class(errors), pointer :: errmgr
+        type(errors), target :: deferr
+        type(plot_data_2d) :: pdata
+        type(plot_data_error_bars) :: edata
+        integer(int32) :: i, k, nparts, nops, flag
+        real(real64), allocatable, dimension(:) :: xd, y, yerr
+        type(color) :: clr
+        
+        ! Initialization
+        if (present(err)) then
+            errmgr => err
+        else
+            errmgr => deferr
+        end if
+        nparts = size(x, 1)
+        nops = size(x, 3)
+        allocate(xd(nparts), stat = flag)
+        if (flag == 0) allocate(y(nparts), stat = flag)
+        if (flag == 0) allocate(yerr(nparts), stat = flag)
+        if (flag /= 0) then
+            call errmgr%report_error("plot_grr", &
+                "Insufficient memory available.", MS_OUT_OF_MEMORY_ERROR)
+            return
+        end if
+
+        ! Establish an x-axis
+        do i = 1, nparts
+            xd(i) = real(i)
+        end do
+
+        ! Cycle over each operator, and generate the plot data
+        do k = 1, nops
+            ! Compute the mean of each parts set, and determine the range
+            do i = 1, nparts
+                y(i) = mean(x(i,:,k))
+                yerr(i) = maxval(x(i,:,k)) - minval(x(i,:,k))
+            end do
+
+            ! Define the plot data
+            call pdata%define_data(xd, y)
+            call pdata%set_line_style(LINE_DOTTED)
+            call plt%push(pdata)
+            clr = pdata%get_line_color()
+
+            call edata%define_y_error_data(xd, y, yerr)
+            call edata%set_name("Operator " // to_string(k))
+            call edata%set_line_color(clr)
+            call plt%push(edata)
+
+            !  Notice, the above code works as a copy of each data object is
+            ! stored and managed by the plot
+        end do
+    end subroutine
 
 ! ------------------------------------------------------------------------------
 end module
