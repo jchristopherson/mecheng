fc6081515970f230269e28c03e6fccd6e88ae734
Jason Christopherson <jchristopherson@hotmail.com>
2019-12-23 13:16:15 -0600

    Update comments

--- src/external/genann.f90
+++ src/external/genann.f90
@@ -3,6 +3,81 @@
 !> @brief This module provides a straight Fortran interface to 
 !! the C library GENANN.  GENANN is availalbe at
 !! https://github.com/codeplea/genann.
+!!
+!! @par Example
+!! The following example illustrates the creating and training of
+!! a simple XOR network.
+!! @code{.f90}
+!! program example
+!!     use iso_c_binding
+!!     use genann_core
+!!     implicit none
+!!
+!!     ! Parameters
+!!     integer(c_int), parameter :: num_inputs = 2
+!!     integer(c_int), parameter :: num_hidden_layers = 1
+!!     integer(c_int), parameter :: num_hidden_neurons = 2
+!!     integer(c_int), parameter :: num_outputs = 1
+!!     integer(c_int), parameter :: num_training = 500
+!!     real(c_double), parameter :: learn_rate = 5.0d0
+!!
+!!     ! Local Variables
+!!     type(c_ptr) :: nn_ptr, optr
+!!     type(genann), pointer :: ann
+!!     real(c_double) :: inputs(2, 4), outputs(1, 4)
+!!     real(c_double), pointer :: rst(:)
+!!     integer(c_int) :: i
+!!
+!!     ! Create the neural network
+!!     nn_ptr = genann_init(num_inputs, num_hidden_layers, &
+!!         num_hidden_neurons, num_outputs)
+!!     if (.not.c_associated(nn_ptr)) then
+!!         print '(A)', "Could not initialize the network."
+!!         return
+!!     end if
+!!     call c_f_pointer(nn_ptr, ann)
+!!
+!!     ! Deffine the expected input and output data for the XOR function
+!!     inputs = reshape([&
+!!         0.0d0, 0.0d0, &
+!!         0.0d0, 1.0d0, &
+!!         1.0d0, 0.0d0, &
+!!         1.0d0, 1.0d0], [2, 4])
+!!     outputs = reshape([0.0d0, 1.0d0, 1.0d0, 0.0d0], [1, 4])
+!!
+!!     ! Train the network
+!!     do i = 1, num_training
+!!         call genann_train(ann, inputs(:,1), outputs(:,1), learn_rate)
+!!         call genann_train(ann, inputs(:,2), outputs(:,2), learn_rate)
+!!         call genann_train(ann, inputs(:,3), outputs(:,3), learn_rate)
+!!         call genann_train(ann, inputs(:,4), outputs(:,4), learn_rate)
+!!     end do
+!!
+!!     ! Display the output of the network
+!!     do i = 1, size(inputs, 2)
+!!         ! Compute the outputs, and get a usable Fortran array
+!!         optr = genann_run(ann, inputs(:,i))
+!!         call c_f_pointer(optr, rst, [num_outputs])
+!!
+!!         ! Display the results
+!!         print '(AF5.3AF5.3AF5.3AF5.3)', &
+!!             "Input 1: ", inputs(1,i), &
+!!             ", Input 2: ", inputs(2, i), &
+!!             ", Output: ", rst(1), &
+!!             ", Expected Output: ", outputs(1,i)
+!!     end do
+!!
+!!     ! Clean up
+!!     call genann_free(ann)
+!! end program
+!! @endcode
+!! The above program produces the following output.
+!! @code{.txt}
+!! Input 1: 0.000, Input 2: 0.000, Output: 0.026, Expected Output: 0.000
+!! Input 1: 0.000, Input 2: 1.000, Output: 0.972, Expected Output: 1.000
+!! Input 1: 1.000, Input 2: 0.000, Output: 0.971, Expected Output: 1.000
+!! Input 1: 1.000, Input 2: 1.000, Output: 0.036, Expected Output: 0.000
+!! @endcode
 module genann_core
     use iso_c_binding
     implicit none
