14942aed8c1678b808b7460eb1573ea31c81db73
Jason Christopherson <jchristopherson@hotmail.com>
2020-01-18 20:05:16 -0600

    Adding to the calibration class

--- src/calibrate/calibrate.f90
+++ src/calibrate/calibrate.f90
@@ -3,18 +3,14 @@
 !> @brief A collection of types and routines supporting calibration operations.
 module calibrate
     use iso_fortran_env
+    use ferror
     use nonlin_polynomials
     implicit none
 
-    !> @brief A type representing a measurement instrument.
-    type instrument
-        !> @brief A description of the instrument
-        character(len = :), allocatable :: description
-        !> @brief the unit of measure for the instrument.
-        character(len = :), allocatable :: unit_type
-        !> @brief The output measurement of the instrument.
-        real(real64) :: measurement
-    end type
+    !> @brief Defines an out-of-memory error condition.
+    integer(int32), parameter :: CAL_OUT_OF_MEMORY_ERROR = 20000
+    !> @brief Defines an invalid input argument error condition.
+    integer(int32), parameter :: CAL_INVALID_INPUT_ERROR = 20001
 
     !> @brief A type representing the environmental conditions during the 
     !! calibration.
@@ -29,8 +25,8 @@ module calibrate
     type calibration
         ! -------------------- !
         !> The calibration data - 2 column matrix where the first column
-        !! contains the output from the UUT, and the second column contains
-        !! the reference standard information.
+        !! contains the output from the reference standard, and the second 
+        !! column contains the UUT output.
         real(real64), private, allocatable, dimension(:,:) :: m_data
         !> @brief The calibration polynomial.
         type(polynomial), private :: m_poly
@@ -47,14 +43,23 @@ module calibrate
         integer(int32), public :: zero_index
 
     contains
-        ! add_point
-        ! remove_last_point
-        ! get/set a calibration data point
-        ! get the number of calibration points
-        ! fit polynomial
         ! evaluate polynomial
         ! compute calibration errors
         ! get polynomial coefficients
+        procedure, public :: initialize => cal_init
+        procedure, public :: get_capacity => cal_get_capacity
+        procedure, public :: set_capacity => cal_set_capacity
+        procedure, public :: get_count => cal_get_count
+        procedure, public :: get => cal_get_data_point
+        generic, public :: set => cal_set_data_point, cal_set_data_point_args
+        generic, public :: append => cal_add_data_point, cal_add_data_point_args
+        procedure, public :: remove_last => cal_remove_last_point
+        procedure, public :: fit_polynomial => cal_fit_poly
+        
+        procedure ::cal_set_data_point
+        procedure :: cal_set_data_point_args
+        procedure :: cal_add_data_point
+        procedure :: cal_add_data_point_args
     end type
 
     interface
@@ -63,6 +68,145 @@ module calibrate
         !! @param[in] this The calibration instance.
         module subroutine cal_init(this)
             class(calibration), intent(inout) :: this
+        end subroutine
+
+        !> @brief Gets the capacity of the calibration instance to accept
+        !! additional data points.
+        !!
+        !! @param[in] this The calibration instance.
+        !! @return The capacity.
+        pure module function cal_get_capacity(this) result(n)
+            class(calibration), intent(in) :: this
+            integer(int32) :: n
+        end function
+
+        !> @brief Sets the capacity of the calibration instance to accept
+        !! additional data points.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] n The desired capacity.  This number must be a positive,
+        !!  non-zero integer.
+        !! @param[in,out] err An optional parameter that is used to track the
+        !!  error status of the routine.  The following error codes are
+        !!  possible.
+        !!  - CAL_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory
+        !!      available.
+        !!  - CAL_INVALID_INPUT_ERROR: Occurs if @p n is not positive and 
+        !!      non-zero.
+        module subroutine cal_set_capacity(this, n, err)
+            class(calibration), intent(inout) :: this
+            integer(int32), intent(in) :: n
+            class(errors), intent(inout), optional, target :: err
+        end subroutine
+
+        !> @brief Gets the current number of data points stored in this object.
+        !!
+        !! @param[in] this The calibration instance.
+        !! @return The number of stored data points.
+        pure module function cal_get_count(this) result(n)
+            class(calibration), intent(in) :: this
+            integer(int32) :: n
+        end function
+
+        !> @brief Gets the requested data point.
+        !!
+        !! @param[in] this The calibration instance.
+        !! @param[in] ind The one-based index of the point to retrieve.
+        !! @return A two-element array containing the requested data point.
+        !!  The first entry in the array contains the reference standard
+        !!  data point, and the second entry in the array contains the UUT
+        !!  data point.
+        pure module function cal_get_data_point(this, ind) result(x)
+            class(calibration), intent(in) :: this
+            integer(int32), intent(in) :: ind
+            real(real64) :: x(2)
+        end function
+
+        !> @brief Replaces the specified data point.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] ind The one-based index of the point to replace.
+        !! @param[in] x A two-element array containing the data point.  The
+        !!  first entry in the array contains the reference standard
+        !!  data point, and the second entry in the array contains the UUT
+        !!  data point.
+        module subroutine cal_set_data_point(this, ind, x)
+            class(calibration), intent(inout) :: this
+            integer(int32), intent(in) :: ind
+            real(real64), intent(in) :: x(2)
+        end subroutine
+
+        !> @brief Replaces the specified data point.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] ind The one-based index of the point to replace.
+        !! @param[in] std The reference standard value.
+        !! @param[in] uut The unit-under-test value.
+        module subroutine cal_set_data_point_args(this, ind, std, uut)
+            class(calibration), intent(inout) :: this
+            integer(int32), intent(in) :: ind
+            real(real64), intent(in) :: std, uut
+        end subroutine
+
+        !> @brief Appends a new data point onto the end of the calibration
+        !! data collection.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] x A two-element array containing the data point.  The
+        !!  first entry in the array contains the reference standard
+        !!  data point, and the second entry in the array contains the UUT
+        !!  data point.
+        !! @param[in,out] err An optional parameter that is used to track the
+        !!  error status of the routine.  The following error codes are
+        !!  possible.
+        !!  - CAL_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory
+        !!      available.
+        module subroutine cal_add_data_point(this, x, err)
+            class(calibration), intent(inout) :: this
+            real(real64), intent(in) :: x(2)
+            class(errors), intent(inout), optional, target :: err
+        end subroutine
+
+        !> @brief Appends a new data point onto the end of the calibration
+        !! data collection.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] std The reference standard value.
+        !! @param[in] uut The unit-under-test value.
+        !! @param[in,out] err An optional parameter that is used to track the
+        !!  error status of the routine.  The following error codes are
+        !!  possible.
+        !!  - CAL_OUT_OF_MEMORY_ERROR: Occurs if there is insufficient memory
+        !!      available.
+        module subroutine cal_add_data_point_args(this, std, uut, err)
+            class(calibration), intent(inout) :: this
+            real(real64), intent(in) :: std, uut
+            class(errors), intent(inout), optional, target :: err
+        end subroutine
+
+        !> @brief Removes the last data point from the calibration data
+        !! collection.
+        !!
+        !! @param[in,out] this The calibration instance.
+        module subroutine cal_remove_last_point(this)
+            class(calibration), intent(inout) :: this
+        end subroutine
+
+        !> @brief Fits a polynomial to the current data set.
+        !!
+        !! @param[in,out] this The calibration instance.
+        !! @param[in] order The order of polynomial to fit.  This value must
+        !!  at least one less than the number of stored data points, and be at
+        !!  least one.
+        !! @param[in,out] err An optional parameter that is used to track the
+        !!  error status of the routine.  The following error codes are
+        !!  possible.
+        !!  - CAL_INVALID_INPUT_ERROR: Occurs if @p order is too large, or less
+        !!      than one.
+        module subroutine cal_fit_poly(this, order, err)
+            class(calibration), intent(inout) :: this
+            integer(int32), intent(in)  :: order
+            class(errors), intent(inout), optional, target :: err
         end subroutine
     end interface
 end module
